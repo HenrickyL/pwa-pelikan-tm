6cf88d259cce31ae6f2fd03e183c4854a80156ee
HenrickyL <henrickyL1@gmail.com>
2020-08-12 23:47:36 -0300

    ended search page

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -6,8 +6,8 @@ let btNext  = null
 export function loadButons(){
     btReturn = document.querySelector(".bt-return")
     btOptions = document.querySelector(".bt-options")
-    // btBack =document.querySelector(".bt-back")
-    // btNext =document.querySelector(".bt-next")
+    btBack =document.querySelector(".bt-back")
+    btNext =document.querySelector(".bt-next")
     console.log("Loaded buttons!")
 }
 export function activateButtons(){
@@ -18,11 +18,29 @@ export function activateButtons(){
         // window.location = "/search"
         console.log("Options Clicked")
     })
+    /////////////////////////////////////
+    let url = window.location.href
+    let aux = url.split('/')
+    let page = Number(aux[aux.length-1])
+    btBack.addEventListener("click",()=>{
+        if( (page-20) >= 0 )
+            window.location = url.slice(0,url.length-String(page).length)+(page-20)
+        else
+        window.location = url.slice(0,url.length-String(page).length)+0
+    })
+    
+    btNext.addEventListener("click",()=>{
+        
+        window.location = url.slice(0,url.length - String(page).length)+(page+20)
+    })
+    
+    
+
     console.log("activated buttons")
+}
 
-
-
-
+function urlAdjust(url){
+    
 }
 
 const header = {loadButons,activateButtons}
--- public/scripts/search.js
+++ public/scripts/search.js
@@ -2,10 +2,23 @@ import header from "./header.js"
 
 window.addEventListener("load",()=>{
     header.loadButons()
+    header.activateButtons()  
+    
+    let btInput = document.querySelector("input")
+    btInput = addEventListener("keyup",(event)=>{
+        if(event.key === "Enter"){
+            window.location = "/search="+event.target.value+"/0"
+        }
+        console.log("input Loaded!")
+    })
+    
+    document.querySelector(".bt-search").addEventListener("click",()=>{
+        window.location = "/search="+btInput.value+"/0"
+    })
 
-    header.activateButtons()
-    
-    
+
+
+
     console.log("<Page Loaded!>")
 
 })
