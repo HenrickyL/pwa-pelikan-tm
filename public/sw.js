035591285a27e0a00d0274fe4634aca9bb724703
HenrickyL <henrickyL1@gmail.com>
2020-08-14 12:15:59 -0300

    offline upgrade

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ const staticCacheName = 'site-static-v1';
 try {
   self.addEventListener('install', function(e) {
     e.waitUntil(
-      caches.open('airhorner').then(function(cache) {
+      caches.open(cacheName).then(function(cache) {
         return cache.addAll([
           '/',
           '/img/',
@@ -62,6 +62,54 @@ try {
 } catch (error) {
   console.log("Error to fetch SW : ",error)
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+
+const offlineFallbackPage = "off_index.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
 
 
 
